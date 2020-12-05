bdbee65548cb9178dd844acc3c2b24a04bfcad44
ellikamishra <ellikamishra009@gmail.com>
2020-12-05 12:24:56 +0530

    trail2

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app=express();
 const PORT=process.env.PORT||3001; 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("./"));
 const db=mysql.createPool({
 
     host: 'localhost',
@@ -24,11 +26,11 @@ const db=mysql.createPool({
 app.use(bodyParser.urlencoded({extended: true}));
 app.post('/api/insert',(req,res)=>
 {
-    const name=req.body.name
-    const address=req.body.address
-    const pswd=req.body.password
-    const contact=req.body.contact
-    const role=req.body.role
+    const name=req.body.name;
+    const address=req.body.address;
+    const pswd=req.body.password;
+    const contact=req.body.contact;
+    
     db.getConnection(function(error, connection) {
         // execute query
         // ...
@@ -38,8 +40,17 @@ app.post('/api/insert',(req,res)=>
     
     }
     else{    
-    connection.query('INSERT INTO Users(name,pswd,address,contact,role) VALUES (?,?,?,?,?)',[name,pswd,address,contact,role],(err,result)=>{
-        console.log(err);
+    connection.query('INSERT INTO Users(name,pswd,address,contact) VALUES (?,?,?,?)',[name,pswd,address,contact],(err,result)=>{
+        if(err){
+            return  res.send({err:err});
+            console.log(err);
+            }
+            if(result){
+            console.log(result);
+            res.send(result);
+            
+           }
+
     });
     }
       });
@@ -48,9 +59,9 @@ app.post('/api/insert',(req,res)=>
 });
 app.post('/api/login',(req,res)=>
 {
-    const name=req.body.name
+    const name=req.body.name;
     
-    const pswd=req.body.password
+    const pswd=req.body.password;
     
     const role=req.body.role
     db.getConnection(function(error, connection) {
@@ -62,7 +73,44 @@ app.post('/api/login',(req,res)=>
     
     }
     else{    
-    connection.query('SELECT* from Users where name = ? and pswd = ? ',[name,pswd],(err,result)=>{
+    connection.query(`SELECT* from Users where name = ? and pswd = ? `,[name,pswd],(err,result,fields)=>{
+        if(err){
+        return  res.send({err:err});
+        console.log(err);
+        }
+        if(result){
+            console.log(result);
+        res.json(result);
+        
+       }
+        else{
+         res.send({message:"Invalid credentials!"});  
+        }
+    });
+    }
+      });
+      //res.send('hello');
+
+});
+app.post('/api/cart',(req,res)=>
+{
+    const no_of_items=Number(req.body.no_of_items);
+    
+    const cost_due=Number(req.body.cost_due);
+    const cust_name=req.body.cust_name;
+    const cust_id=Number(req.body.cust_id);
+    console.log(cost_due)
+    console.log(no_of_items)
+    db.getConnection(function(error, connection) {
+        // execute query
+        // ...
+    if(error)
+    {
+        console.log(error);
+    
+    }
+    else{    
+    connection.query('INSERT INTO orders(no_of_items,cost_due,cust_name,cust_id) VALUES (?,?,?,?)',[no_of_items,cost_due,cust_name,cust_id],(err,result)=>{
         if(err){
         return  res.send({err:err});
         console.log(err);
@@ -79,6 +127,7 @@ app.post('/api/login',(req,res)=>
       //res.send('hello');
 
 });
+
 app.listen(PORT,(req,res)=>{                          //app listening to port (localhost),using postman app to get and post requests
     console.log(`listening to port ${PORT}`);
 })
